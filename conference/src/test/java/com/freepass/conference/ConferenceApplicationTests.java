9d60480c8b1d4d72296a70c93c47a5b1c9952455
WhateverAWS <151141957+WhateverAWS@users.noreply.github.com>
2025-01-25 14:37:32 +0700

    create project

---
+++ conference/src/test/java/com/freepass/conference/ConferenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freepass.conference;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
